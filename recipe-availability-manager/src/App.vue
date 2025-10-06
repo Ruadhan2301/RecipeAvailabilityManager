9c1f1ebd665c217407501d62debd34a38b6a41f3
Ruadhan2301 <49420750+Ruadhan2301@users.noreply.github.com>
2025-10-06 20:01:39 +0100

    Initial commit

---
+++ recipe-availability-manager/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div class="body">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+header{
+  background-color: #42b983;
+  padding: 1rem 0;
+  color: white;
+  margin-bottom: 2rem;
+}
+nav{
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+}
+</style>
